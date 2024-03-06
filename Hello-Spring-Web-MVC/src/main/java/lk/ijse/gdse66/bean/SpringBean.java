3c28284bc746ec464cd30bb811b6852dd335d7a6
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-06 20:51:12 +0530

    Refactored 2

--- Hello-Spring-Web-MVC/src/main/java/lk/ijse/gdse66/bean/SpringBean.java
+++ Hello-Spring-Web-MVC/src/main/java/lk/ijse/gdse66/bean/SpringBean.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
     public SpringBean() {
-        System.out.println("SpringBean()");
+        System.out.println("SpringBean.()");
     }
 
     @Override
