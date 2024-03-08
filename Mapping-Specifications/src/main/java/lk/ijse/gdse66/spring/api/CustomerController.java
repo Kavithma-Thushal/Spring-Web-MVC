c860544cf944d27b57ddb3d8ddc9778759d91ef1
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-08 12:26:23 +0530

    Mapping-Specifications Module Refactored 2

---
+++ Mapping-Specifications/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse66.spring.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Spring-Web-MVC
+ * @since : 10:34 AM - 3/8/2024
+ **/
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @GetMapping("/search")
+    public String getName() {
+        System.out.println("Customer Name - Thushal");
+        return "Customer Name - Thushal";
+    }
+}
