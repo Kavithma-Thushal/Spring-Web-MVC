a01b46f0d0e1bdae7e00101c61db89073d861d04
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-03-09 14:17:54 +0530

    AdminController Added

---
+++ Send-Data/src/main/java/lk/ijse/gdse66/spring/api/AdminController.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse66.spring.api;
+
+import lk.ijse.gdse66.spring.dto.AdminDTO;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : Spring-Web-MVC
+ * @since : 1:47 PM - 3/9/2024
+ **/
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    /*Accept application/x-www-form-urlencoded or query parameters*/
+    @PostMapping
+    public void saveAdmin(@ModelAttribute AdminDTO adminDTO) {
+        System.out.println(adminDTO.toString());
+    }
+}
